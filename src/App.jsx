eaacac98bd36bd6a71ab68952bb4820ecd68d640
manjunagaraj95-sudo <manjunagaraj95@gmail.com>
2026-02-13 17:28:20 +0530

    Delete src/App.jsx

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>🚀 Prototype deployed from Appian</h1>
-      <p>If you see this, deployment works.</p>
-    </div>
-  );
-}
