a19522a18f51bb09253427a1635adc3396d5fe9f
manjunagaraj95-sudo <manjunagaraj95@gmail.com>
2026-02-13 17:11:35 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>🚀 Prototype deployed from Appian</h1>
+      <p>If you see this, deployment works.</p>
+    </div>
+  );
+}
